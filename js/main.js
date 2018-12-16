cba814b4d1952a4fa716700b84999ebafc153fc2
dejanrojin <dejan.rojin@yahoo.com>
2018-12-17 00:16:27 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+
+
+$("#add").click(function(){
+var money_given=$("#moneyg").val();
+var qnty=$("#qnty").val();
+var price=$("#price").val();
+
+
+var ukupno=parseInt(qnty) * parseInt(price);
+
+$("#total").text("Ukupno: "+parseInt(ukupno)+"Din");
+
+
+var kusur=parseInt(money_given) - parseInt(ukupno);
+
+$("#return").text("Kusur: "+parseInt(kusur)+"Din");
+
+
+});
+
+
+
+
+
+$("#reset").click(function(){
+
+
+
+
+
+var money_given=$("#moneyg").val();
+var qnty=$("#qnty").val();
+var price=$("#price").val();
+var qnty=$("#total").val();
+var price=$("#return").val();
+
+money_given=$("#moneyg").val("");
+qnty=$("#qnty").val("");
+price=$("#price").val("");
+qnty=$("#total").val("");
+ price=$("#return").val("");
+
+ 
+ 
+ $("#total").text("Ukupno: ");
+
+$("#return").text("Kusur: ");
+
+});
+
+
+
+
+});
